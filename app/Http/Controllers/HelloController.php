d65d3b9f4cad437de6f96398b2595f074301218c
tokie-inomata <asahane06@gmail.com>
2021-03-13 11:25:16 +0900

    63ページまで

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+    public function index(){
+        $data = ['msg' =>'これはコントローラから渡されたメッセージです。'];
+        return view('hello.index', $data);
+    }
+}       
--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body  {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:rifht; color:#eee;
-    margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>This is sample page.</p>
-    <p>これは、サンプルで作ったページです。</p>
-</body>
-EOF;
-
-Route::get('hello',function () use ($html) {
-    return $html;
-});
+Route::get('hello', 'HelloController@index');
