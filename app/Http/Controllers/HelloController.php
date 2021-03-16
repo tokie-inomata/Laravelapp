0381cdbba94b3698d6911477772a1fdaab029389
tokie-inomata <asahane06@gmail.com>
2021-03-16 21:38:53 +0900

    3月16日190ページまで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,16 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['msg'=>'フォームを入力:']);
+        if ($request->hasCookie('msg'))
+        {
+            $msg = 'Cookie: ' . $request->cookie('msg');
+        } else {
+            $msg = '※クッキーはありません。';
+        }
+        return view('hello.index', ['msg'=>$msg]);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        return view('hello.index', ['msg'=>'正しく入力されました！']);
+        $validate_rule = [
+            'msg' => 'required',
+        ];
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = response()->view('hello.index', 
+            ['msg'=>'「'. $msg. '」をクッキーに保存しました。']);
+        $response->cookie('msg', $msg, 100);
+        return $response;
+
     }
 }
--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -3,12 +3,13 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\Myrule;
 
 class HelloRequest extends FormRequest
 {
     public function authorize()
     {
-        if($this->pach() == 'hello')
+        if ($this->path() == 'hello')
         {
             return true;
         } else {
@@ -21,7 +22,17 @@ class HelloRequest extends FormRequest
         return [
             'name' => 'required',
             'mail' => 'email',
-            'age' => 'numeric|between:0,150',
+            'age' =>  ['numeric', new Myrule(5)],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入下さい。',
+            'age.hello' => 'Hello! 入力は偶数のみ受け付けます。'
         ];
     }
 }
