3a0ed7e347f5555bd733437c423e97b975dbb94d
tokie-inomata <asahane06@gmail.com>
2021-03-20 01:02:37 +0900

    3月19日教本234ページまで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,7 +12,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
+        $items = DB::table('people')->orderBy('age', 'asc')->get();
         return view('hello.index', ['items' => $items]);
     }
 
@@ -34,7 +34,51 @@ class HelloController extends Controller
             'mail' => $request->mail,
             'age'  => $request->age ,
         ];
-        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)',$param);
+        DB::table('people')->insert($param);
         return redirect('/hello');
+    }
+
+    public function edit(Request $request)
+    {
+        $item = DB::table('people')
+            ->where('id', $request->id)->first();
+        return view('hello.edit', ['form' => $item]);
+    }
+
+    public function update(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::table('people')
+            ->where('id', $request->id)
+            ->update($param);
+        return redirect('/hello');
+    }
+
+    public function del(Request $request)
+    {
+        $item = DB::table('people')
+            ->where('id', $request->id)->first();
+        return view('hello.del', ['form' => $item]);
+    }
+
+    public function remove(Request $request)
+    {
+        DB::table('people')
+            ->where('id', $request->id)->delete();
+        return redirect('/hello');
+    }
+
+    public function show(Request $request)
+    {
+        $page = $request->page;
+        $items = DB::table('people')
+            ->offset($page * 3)
+            ->limit(3)
+            ->get();
+        return view('hello.show', ['items' => $items]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,11 @@ Route::get('/', function () {
 
 use App\Http\Middleware\HelloMiddleware;
 
+Route::get('hello', 'HelloController@index');
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+Route::get('hello/show', 'HelloController@show');
