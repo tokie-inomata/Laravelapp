a95f5b65b2206b8b741a2572be1cbd937f8f032a
tokie-inomata <asahane06@gmail.com>
2021-03-21 22:41:03 +0900

    3月21日教本282ページまで。belongsTo結合でエラーを出した。

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 use App\Http\Middleware\HelloMiddleware;
 
+
+//DBに登録、更新、削除の動作コントローラ
 Route::get('hello', 'HelloController@index');
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
@@ -25,3 +27,19 @@ Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
 Route::get('hello/show', 'HelloController@show');
+
+//Eloquentの動作コントローラ
+Route::get('person', 'PersonController@index');
+Route::get('person/find', 'PersonController@find');
+Route::post('person/find', 'PersonController@search');
+Route::get('person/add', 'PersonController@add');
+Route::post('person/add', 'PersonController@create');
+Route::get('person/edit', 'PersonController@edit');
+Route::post('person/edit', 'PersonController@update');
+Route::get('person/del', 'PersonController@delete');
+Route::post('person/del', 'PersonController@remove');
+
+//Boardのルート
+Route::get('board', 'BoardController@index');
+Route::get('board/add', 'BoardController@add');
+Route::post('board/add', 'BoardController@create');
